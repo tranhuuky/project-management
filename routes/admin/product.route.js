5cc937463700a49733b4e3496dffa1b450210fd7
tranhuuky <huukyhk1234@gmail.com>
2024-10-10 22:14:23 +0700

    Đẩy file tĩnh lên Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const multer = require('multer');
+
 const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+
+const upload = multer();
+
+
 const router = express.Router();
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product-validate")
 
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -16,6 +22,7 @@ router.get("/create", controller.create);
 router.post(
     "/create",
     upload.single('thumbnail'),
+    uploadCloud.upload,
     validate.createPost,
     controller.createPost
 );
